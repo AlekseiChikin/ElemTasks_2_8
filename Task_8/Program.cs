e14629157abee952ad32ece20793b5af709a805c
Aleksei Chikin <lehafar@gmail.com>
2021-11-21 15:00:05 +0200

    refactor: move clas fibonacci from main to another file

--- Task_8/Program.cs
+++ Task_8/Program.cs
@@ -5,46 +5,7 @@ using System.Linq;
 using System.Numerics;
 namespace Task_8
 {
-    class Fibonacci
-    {
-        private List<BigInteger> _sequence;
-        private readonly double _goldenRatio;
-        public Fibonacci()
-        {
-            _goldenRatio = (Math.Sqrt(5) + 1) / 2;
-            _sequence = new List<BigInteger>() { 0, 1 };
-        }
-        public List<BigInteger> GetNthFibonacci(int n) //private
-        {
-            int number = n ; 
-            
-            for (int i = 2; i <= number; i++)
-            {
-                _sequence.Add(_sequence[i - 2] + _sequence[i - 1]);
-            }
-            return _sequence;
-        }
-        public IEnumerable<BigInteger> RangeFib(int min, int max)
-        {
-            return _sequence.Where(x => x >= min && x <= max);
-        }
-        public IEnumerable<BigInteger> LenghtFib(int len)
-        {
-            return _sequence.Where(x => x.ToString().Length == len);
-        }
-        public double GetElemFromIndex(double index)
-        {
-            return Math.Round(Math.Pow(_goldenRatio, index) / Math.Sqrt(5));
-        }
-        public double GetIndexFromElem(double elem) 
-        {
-            return Math.Round(Math.Log10(elem * Math.Sqrt(5) + 0.5) / Math.Log10(_goldenRatio));
-        }
-        public double BinetsFormula(double index)
-        {
-            return Math.Round((Math.Pow(_goldenRatio, index) - Math.Pow(-_goldenRatio, -index)) / Math.Sqrt(5));
-        }
-    }
+    
     class Program
     {
     
@@ -52,7 +13,7 @@ namespace Task_8
         {
            
 
-            Fibonacci fibon = new Fibonacci();
+            FibonacciNumbers fibon = new FibonacciNumbers();
             List<BigInteger> list = fibon.GetNthFibonacci(100);
             foreach (var item in list)
             {
